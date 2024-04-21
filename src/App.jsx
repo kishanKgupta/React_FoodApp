81eda1a29a3dcfe334a6934bb48341c391ee1890
kishan gupta <kkgupta0106@gmail.com>
2024-04-22 02:01:35 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>kishan kumar gupta</h1>
+      <h2>My first react PROJECT</h2>
+    </>
+  )
+}
+
+export default App
